abd135b7aedabd3bc2e523150e68dbf72e9d9a7e
MrClock8163 <jz.zalan@gmail.com>
2023-06-04 14:31:11 +0200

    Added Align With Object proxy operator

--- Arma3ObjectBuilder/ui/proxies.py
+++ Arma3ObjectBuilder/ui/proxies.py
@@ -1,27 +1,58 @@
 import os
 
 import bpy
+import mathutils
 
 from ..utilities import proxy as proxyutils
 from ..io import import_p3d
+
+
+class A3OB_OT_proxy_realign_ocs(bpy.types.Operator):
+    """Realign the proxy object coordinate system with proxy directions"""
+    
+    bl_idname = "a3ob.proxy_realign_ocs"
+    bl_label = "Realign Coordinate System"
+    bl_options = {'UNDO'}
+    
+    @classmethod
+    def poll(cls, context):
+        obj = context.active_object
+        return obj and len(context.selected_objects) == 1 and obj.type == 'MESH' and obj.a3ob_properties_object_proxy.is_a3_proxy
+    
+    def execute(self, context):
+        obj = context.active_object
+        import_p3d.transform_proxy(obj)
+            
+        return {'FINISHED'}
 
 
 class A3OB_OT_proxy_align(bpy.types.Operator):
     """Align the proxy object coordinate system with proxy directions"""
     
     bl_idname = "a3ob.proxy_align"
-    bl_label = "Align Coordinate System"
+    bl_label = "Align With Object"
     bl_options = {'UNDO'}
     
     @classmethod
     def poll(cls, context):
         obj = context.active_object
-        return obj and obj.type == 'MESH' and obj.a3ob_properties_object_proxy.is_a3_proxy
+        selected = context.selected_objects
+        
+        if not obj or len(selected) != 2:
+            return False
+            
+        selected.remove(obj)
+        return obj.mode == 'OBJECT' and selected[0] and selected[0].mode == 'OBJECT' and selected[0].a3ob_properties_object_proxy.is_a3_proxy
     
     def execute(self, context):
         obj = context.active_object
-        import_p3d.transform_proxy(obj)
-            
+        selected = context.selected_objects.copy()
+        selected.remove(obj)
+        proxy = selected[0]
+        
+        proxy.matrix_world = obj.matrix_world
+        proxy.scale = mathutils.Vector((1, 1, 1))
+                    
         return {'FINISHED'}
 
 
@@ -72,7 +103,7 @@ class A3OB_OT_proxy_extract(bpy.types.Operator):
     @classmethod
     def poll(cls, context):
         obj = context.active_object
-        return obj and obj.type == 'MESH' and obj.a3ob_properties_object_proxy.is_a3_proxy and os.path.exists(obj.a3ob_properties_object_proxy.proxy_path) and os.path.splitext(obj.a3ob_properties_object_proxy.proxy_path)[1].lower() == '.p3d'
+        return obj and obj.type == 'MESH' and len(context.selected_objects) == 1 and obj.a3ob_properties_object_proxy.is_a3_proxy and os.path.exists(obj.a3ob_properties_object_proxy.proxy_path) and os.path.splitext(obj.a3ob_properties_object_proxy.proxy_path)[1].lower() == '.p3d'
     
     def execute(self, context):
         proxy_object = context.active_object
@@ -107,12 +138,14 @@ class A3OB_PT_proxies(bpy.types.Panel):
     def draw(self, context):
         layout = self.layout
         
-        layout.operator("a3ob.proxy_align", icon='EMPTY_AXIS')
+        layout.operator("a3ob.proxy_align", icon='CUBE')
+        layout.operator("a3ob.proxy_realign_ocs", icon='ORIENTATION_GLOBAL')
         layout.operator("a3ob.proxy_extract", icon='IMPORT')
 
 
 classes = (
     A3OB_OT_proxy_align,
+    A3OB_OT_proxy_realign_ocs,
     A3OB_OT_proxy_extract,
     A3OB_PT_proxies
 )
