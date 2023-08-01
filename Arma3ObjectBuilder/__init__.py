b61fc8b04cb799588fb486e045b9f09122b15728
MrClock8163 <jz.zalan@gmail.com>
2023-08-02 01:00:17 +0200

    Incremented version

--- Arma3ObjectBuilder/__init__.py
+++ Arma3ObjectBuilder/__init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Arma 3 Object Builder",
     "description": "Collection of tools for editing Arma 3 content",
     "author": "MrClock (present add-on), Hans-Joerg \"Alwarren\" Frieden (original ArmaToolbox add-on)",
-    "version": (0, 3, 4),
+    "version": (0, 3, 5, "rc"),
     "blender": (2, 90, 0),
     "location": "Object Builder panels",
     "warning": "Work In Progress",
