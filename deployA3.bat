316e900a496c7c3d5b02800654f7fe5ef45bda72
MrClock <48679831+MrClock8163@users.noreply.github.com>
2023-08-11 01:56:54 +0200

    Delete deployA3.bat

--- deployA3.bat
+++
@@ -1,4 +0,0 @@
-xcopy /y /e "Arma3ObjectBuilder" "%appdata%\Blender Foundation\Blender\2.90\scripts\addons\Arma3ObjectBuilder"
-xcopy /y /e "Arma3ObjectBuilder" "%appdata%\Blender Foundation\Blender\3.6\scripts\addons\Arma3ObjectBuilder"
-del /s/q "%appdata%\Blender Foundation\Blender\2.90\scripts\addons\Arma3ObjectBuilder\__pycache__"
-del /s/q "%appdata%\Blender Foundation\Blender\3.6\scripts\addons\Arma3ObjectBuilder\__pycache__"
