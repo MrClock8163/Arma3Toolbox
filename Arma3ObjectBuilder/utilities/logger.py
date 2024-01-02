4a2644b0743cd3dd1ce4b2e3b42a22406a965c51
MrClock8163 <jz.zalan@gmail.com>
2024-01-02 01:44:39 +0100

    Added null logger

--- Arma3ObjectBuilder/utilities/logger.py
+++ Arma3ObjectBuilder/utilities/logger.py
@@ -4,8 +4,8 @@
 
 class ProcessLogger():
     
-    def __init__(self, startIndent = 0):
-        self.indent = startIndent
+    def __init__(self, start_indent = 0):
+        self.indent = start_indent
         
     def level_up(self, message = ""):
         self.indent += 1
@@ -24,5 +24,22 @@ class ProcessLogger():
         if time > -1:
             print("\t" * (self.indent + 1), "Done in %f sec" % time, sep="")
             
-    def log(self, message=""):
+    def log(self, message = ""):
         print("\t" * (self.indent + 1), message, sep="")
+
+
+class ProcessLoggerNull():
+    def __init__(self, start_indent = 0):
+        pass
+
+    def level_up(self, message = ""):
+        pass
+
+    def level_down(self, message = ""):
+        pass
+
+    def step(self, message, time = -1):
+        pass
+
+    def log(self, message = ""):
+        pass
