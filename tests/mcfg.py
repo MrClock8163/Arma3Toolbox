e377b8b0acbbd0b177dda6d3f22da94830257f19
MrClock <jz.zalan@gmail.com>
2024-02-13 23:47:08 +0100

    First batch of testing helper functions

---
+++ tests/mcfg.py
@@ -0,0 +1,62 @@
+"""
+blender -b -noaudio --python tests/mcfg.py
+"""
+
+
+import os
+import unittest
+
+import bpy
+
+
+folder_inputs = os.path.join(os.getcwd(), "tests/inputs")
+folder_outputs = os.path.join(os.getcwd(), "tests/outputs/mcfg")
+
+
+class McfgTest(unittest.TestCase):
+    """Test cases to test model.cfg handling"""
+
+    def test_import(self):
+        """Import sample model.cfg and verify length of contents"""
+
+        bpy.ops.wm.read_homefile(app_template="")
+
+        file_in_mcfg = os.path.join(folder_inputs, "model.cfg")
+
+        bpy.ops.a3ob.import_mcfg(filepath=file_in_mcfg)
+        
+        scene_props = bpy.context.scene.a3ob_rigging
+        self.assertEqual(len(scene_props.skeletons), 1)
+        self.assertEqual(len(scene_props.skeletons[0].bones), 18)
+    
+    def test_export(self):
+        """Create skeleton definition and export to model.cfg"""
+
+        bpy.ops.wm.read_homefile(app_template="")
+
+        file_out_mcfg = os.path.join(folder_outputs, "model.cfg")
+        scene_props = bpy.context.scene.a3ob_rigging
+
+        skeleton = scene_props.skeletons.add()
+        skeleton.name = "Skeleton"
+
+        bone_base = skeleton.bones.add()
+        bone_base.name = "bone_0"
+
+        for i in range(10):
+            bone = skeleton.bones.add()
+            bone.name = ("bone_%d" % (i + 1))
+            bone.parent = "bone_0"
+        
+        bpy.ops.a3ob.export_mcfg(filepath=file_out_mcfg, skeleton_index=0)
+
+
+def main():
+    if not os.path.isdir(folder_outputs):
+        os.makedirs(folder_outputs)
+
+    unittest.main(argv=["blender"])
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/p3d.py
@@ -0,0 +1,64 @@
+"""
+blender -b -noaudio --python tests/p3d.py
+"""
+
+
+import os
+import unittest
+
+import bpy
+
+
+folder_inputs = os.path.join(os.getcwd(), "tests/inputs/p3d")
+folder_outputs = os.path.join(os.getcwd(), "tests/outputs/p3d")
+
+
+class P3DTest(unittest.TestCase):
+    """Test cases to test P3D related functionalities"""
+
+    def __init__(self, methodName = "runTest"):
+        self.inputs = [file for file in os.listdir(folder_inputs) if os.path.splitext(file)[1].lower() == ".p3d"]
+        super().__init__(methodName)
+
+    def test_import(self):
+        """Import the sample models"""
+
+        for file in self.inputs:
+            file_in_p3d = os.path.join(folder_inputs, file)
+
+            bpy.ops.wm.read_homefile(app_template="")
+            bpy.ops.a3ob.import_p3d(filepath=file_in_p3d)
+    
+    def test_import_export(self):
+        """Import -> export the sample models, re-import -> re-export the first exported models, and compare size"""
+
+        for file in self.inputs:
+            name = os.path.splitext(file)[0]
+
+            file_in_p3d = os.path.join(folder_inputs, file)
+            file_out_p3d_1 = os.path.join(folder_outputs, name + "_out.p3d")
+            file_out_p3d_2 = os.path.join(folder_outputs, name + "_out_re.p3d")
+            
+            bpy.ops.wm.read_homefile(app_template="")
+            bpy.ops.a3ob.import_p3d(filepath=file_in_p3d)
+            bpy.ops.a3ob.export_p3d(filepath=file_out_p3d_1)
+            bpy.ops.wm.read_homefile(app_template="")
+            bpy.ops.a3ob.import_p3d(filepath=file_out_p3d_1)
+            bpy.ops.a3ob.export_p3d(filepath=file_out_p3d_2)
+
+            size_out = os.path.getsize(file_out_p3d_1)
+            size_out_re = os.path.getsize(file_out_p3d_2)
+            self.assertEqual(size_out, size_out_re, "%s (%d bytes) and %s (%d bytes) are not the same size" % (file_out_p3d_1, size_out, file_out_p3d_2, size_out_re))
+
+
+def main():
+    if not os.path.isdir(folder_inputs):
+        os.makedirs(folder_inputs)
+    if not os.path.isdir(folder_outputs):
+        os.makedirs(folder_outputs)
+        
+    unittest.main(argv=["blender"])
+
+
+if __name__ == "__main__":
+    main()
